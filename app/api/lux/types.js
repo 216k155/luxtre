bece39c942876d1ef9a524de8677300bbf930ade
216k155 <216k155@luxcore.io>
2018-02-25 17:14:50 +1100

    export luxinfo - getinfo

--- app/api/lux/types.js
+++ app/api/lux/types.js
@@ -42,3 +42,28 @@ export type LuxTransactions = {
   received: Array<LuxTransaction>,
   sent: Array<LuxTransaction>,
 };
+
+export type LuxInfo = {
+		//version: number,
+		//protocolversion: number,
+		//walletversion: number,
+		//balance: BigNumber,
+		blocks: number,
+		//timeoffset: number,
+		//connections: number,
+		//proxy: string,
+		//difficulty: BigNumber//3007383866429.732,
+		//testnet: boolean,
+		//keypoololdest: number,
+		//keypoolsize: number,
+		//unlocked_until: number,
+		//paytxfee: BigNumber,
+		//relayfee: BigNumber,
+		errors: string
+};
+
+export type LuxPeerInfo = {
+  startingheight: number
+};
+
+export type LuxPeerInfos = Array<LuxPeerInfo>;
