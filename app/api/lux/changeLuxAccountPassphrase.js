8ebdd8b4f1a043710a1b7abeb2a67830b0c3d14c
216k155 <216k155@luxcore.io>
2018-02-26 09:30:54 +1100

    auth lux usr & pwd

--- app/api/lux/changeLuxAccountPassphrase.js
+++ app/api/lux/changeLuxAccountPassphrase.js
@@ -1,29 +1,25 @@
 // @flow
 import { request } from './lib/request';
-import { LUX_API_HOST, LUX_API_PORT } from './index';
+import { LUX_API_HOST, LUX_API_PORT, LUX_API_USER, LUX_API_PWD } from './index';
 import type { LuxAccountPassphrase } from './types';
 
 export type ChangeLuxAccountPassphraseParams = {
-  ca: string,
-  walletId: string,
   oldPassword: ?string,
   newPassword: ?string,
 };
 
 export const changeLuxAccountPassphrase = (
-  { ca, walletId, oldPassword, newPassword }: ChangeLuxAccountPassphraseParams
+  { oldPassword, newPassword }: ChangeLuxAccountPassphraseParams
 ): Promise<LuxAccountPassphrase> => (
   request({
     hostname: LUX_API_HOST,
     method: 'POST',
-    path: '/',
     port: LUX_API_PORT,
-    ca,
+    auth: LUX_API_USER + ':' + LUX_API_PWD
   }, {
     jsonrpc: '2.0',
-    method: 'daedalus_changePassphrase',
+    method: 'walletpassphrasechange',
     params: [
-      walletId,
       oldPassword || '',
       newPassword || '',
     ]
