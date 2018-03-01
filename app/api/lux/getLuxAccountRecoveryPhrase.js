b802b6eed76c303e7b005cb726ca9afa553b6845
kaceybolman <kaceybolman>
2018-03-01 10:45:28 +0800

    delete lux

--- app/api/lux/getLuxAccountRecoveryPhrase.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import { generateMnemonic } from '../../utils/crypto';
-import type { LuxRecoveryPassphrase } from './types';
-
-export const getLuxAccountRecoveryPhrase = (): LuxRecoveryPassphrase => (
-  generateMnemonic().split(' ')
-);
