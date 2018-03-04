c86f367698a842fb54477065f441ef7584b6e67a
Eric Juta <ericjohn.juta@consensys.net>
2018-03-04 17:55:06 +0000

    Fix rendering duplicate transactions via new key generation

--- app/components/wallet/transactions/WalletTransactionsList.js
+++ app/components/wallet/transactions/WalletTransactionsList.js
@@ -116,15 +116,7 @@ export default class WalletTransactionsList extends Component<Props> {
             <div className={styles.groupDate}>{this.localizedDate(group.date)}</div>
             <div className={styles.list}>
               {group.transactions.map((transaction, transactionIndex) => (
-                <div
-                  key={
-                    walletId +
-                    '-' +
-                    transaction.id +
-                    transaction.date.toUTCString() +
-                    transaction.numberOfConfirmations
-                  }
-                >
+                <div key={`${walletId}-${transaction.id}-${transaction.type}`}>
                   <Transaction
                     data={transaction}
                     isLastInList={transactionIndex === group.transactions.length - 1}
