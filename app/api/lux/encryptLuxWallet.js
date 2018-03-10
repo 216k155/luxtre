359aeb9ff44562eb8e805d42ef813f980d48cf29
cui wang <mrcui422@gmail.com>
2018-03-10 17:30:57 +0800

    Fixed to refresh transactions.

---
+++ app/api/lux/encryptLuxWallet.js
@@ -0,0 +1,23 @@
+// @flow
+import BigNumber from 'bignumber.js';
+import { request } from './lib/request';
+import { LUX_API_HOST, LUX_API_PORT, LUX_API_USER, LUX_API_PWD } from './index';
+
+export type EncryptLuxWalletParams = {
+  password: string
+};
+
+export const encryptLuxWallet = ({ password }: EncryptLuxWalletParams): Promise<any> =>
+  request(
+    {
+      hostname: LUX_API_HOST,
+      method: 'POST',
+      port: LUX_API_PORT,
+      auth: LUX_API_USER + ':' + LUX_API_PWD
+    },
+    {
+      jsonrpc: '2.0',
+      method: 'encryptwallet',
+      params: [password]
+    }
+  );
