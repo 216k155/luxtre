141c2014ea683350f82821d3a17d072771406251
216k155@luxcore.io <216k155@luxcore.io>
2018-04-30 20:52:23 +0800

    Added terms of use for luxgate.

---
+++ app/containers/profile/TermsOfUseForLuxgatePage.js
@@ -0,0 +1,36 @@
+// @flow
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import TopBar from '../../components/layout/TopBar';
+import TopBarLayout from '../../components/layout/TopBarLayout';
+import TermsOfUseForm from '../../components/profile/terms-of-use/TermsOfUseForm';
+import type { InjectedProps } from '../../types/injectedPropsType';
+
+@inject('stores', 'actions') @observer
+export default class TermsOfUseForLuxgatePage extends Component<InjectedProps> {
+
+  static defaultProps = { actions: null, stores: null };
+
+  onSubmit = () => {
+    this.props.actions.profile.acceptTermsOfUseForLuxgate.trigger();
+  };
+
+  render() {
+    const { setTermsOfUseForLuxgateAcceptanceRequest, termsOfUseForLuxgate } = this.props.stores.profile;
+    const { currentRoute } = this.props.stores.app;
+    const isSubmitting = setTermsOfUseForLuxgateAcceptanceRequest.isExecuting;
+    const topbar = <TopBar currentRoute={currentRoute} />;
+    return (
+      <TopBarLayout
+        topbar={topbar}
+      >
+        <TermsOfUseForm
+          localizedTermsOfUse={termsOfUseForLuxgate}
+          onSubmit={this.onSubmit}
+          isSubmitting={isSubmitting}
+          error={setTermsOfUseForLuxgateAcceptanceRequest.error}
+        />
+      </TopBarLayout>
+    );
+  }
+}
