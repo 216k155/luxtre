723db7d7cb28d01433bcc8f80530b9c4541e5769
216k155 <216k155@luxcore.io>
2018-05-01 19:41:10 +0800

    merged terms and ui

--- app/components/exchange/ExchangePage.js
+++ app/components/exchange/ExchangePage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import ExchangeOrderPage from "./ExchangeOrderPage";
 import ExchangeSettingPage from "./ExchangeSettingPage";
 import styles from "./ExchangePage.scss";
 
