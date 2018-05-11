837fc6273b3b533eb305dbaefc57e299e4d866de
216k155@luxcore.io <216k155@luxcore.io>
2018-05-11 17:01:06 +0800

    Defined password to talk with luxgate rpc.

--- app/api/luxgate/index.js
+++ app/api/luxgate/index.js
@@ -11,6 +11,7 @@ import { getLuxgatePriceArray } from './getLuxgatePriceArray';
 
 export const LUXGATE_API_HOST = 'localhost';
 export const LUXGATE_API_PORT = 9883;
+export const LUXGATE_USER_PASSWORD = "fadce34e546424c31c34eeb0d2c9e6505d9f589cdddf3a00d6bd8e1d7105bf15";
 
 import { 
     Logger, 
@@ -50,7 +51,7 @@ export default class LuxApi {
     async getCoinBalanace(coin: string, address: string): Promise<GetCoinBalanceResponse> {
         Logger.debug('LuxgateApi::getCoinBalanace called');
         try {
-            const password = "610fb5f228b6857fa91ada725033979a2a5e327b9a68ee6c59300d9d31c84718";
+            const password = LUXGATE_USER_PASSWORD;
             const response = await getLuxgateCoinBalanceFromAddress( {coin, password, address} );
             if (response !== undefined && response.result === "success")
             {
@@ -67,7 +68,7 @@ export default class LuxApi {
     async getCoinInfo(coin: string): Promise<GetCoinInfoResponse> {
         Logger.debug('LuxgateApi::getCoinInfo called');
         try {
-            const password = "610fb5f228b6857fa91ada725033979a2a5e327b9a68ee6c59300d9d31c84718";
+            const password = LUXGATE_USER_PASSWORD;
             const response = await getLuxgateCoinInfo({coin, password});
             if (response !== undefined && response.result === "success")
             {
@@ -85,7 +86,7 @@ export default class LuxApi {
         Logger.debug('LuxgateApi::withdraw called');
         const { coin, receiver, amount } = request;
         try {
-            const password = "610fb5f228b6857fa91ada725033979a2a5e327b9a68ee6c59300d9d31c84718";
+            const password = LUXGATE_USER_PASSWORD;
             const response = await withdrawLuxgateCoin({coin, receiver, amount, password});
             if (response !== undefined && response.result === "success")
                 return true;
@@ -100,7 +101,7 @@ export default class LuxApi {
     async getLGOrders(base: string, rel: string): Promise<GetLGOrdersResponse> {
         Logger.debug('LuxgateApi::getLGOrders called');
         try {
-            const password = "610fb5f228b6857fa91ada725033979a2a5e327b9a68ee6c59300d9d31c84718";
+            const password = LUXGATE_USER_PASSWORD;
             const response = await getLuxgateOrders({password, base, rel});
             if (response !== undefined)
             {
@@ -117,7 +118,7 @@ export default class LuxApi {
     async getLGTransactions(coin: string, address: string): Promise<GetLGTransactionsResponse> {
         Logger.debug('LuxgateApi::getLGTransactions called');
         try {
-            const password = "610fb5f228b6857fa91ada725033979a2a5e327b9a68ee6c59300d9d31c84718";
+            const password = LUXGATE_USER_PASSWORD;
             const response = await getLuxgateTransactions({coin, password, address});
             if (response !== undefined && !response.error)
             {
@@ -133,7 +134,7 @@ export default class LuxApi {
     async getLGTradeArray(base: string, rel: string): Promise<GetLGTradeArrayResponse> {
         Logger.debug('LuxgateApi::getLGTradeArray called');
         try {
-            const password = "610fb5f228b6857fa91ada725033979a2a5e327b9a68ee6c59300d9d31c84718";
+            const password = LUXGATE_USER_PASSWORD;
             const timeline = 60;
             const response = await getLuxgateTradeArray({password, base, rel, timeline});
             if (response !== undefined)
@@ -150,7 +151,7 @@ export default class LuxApi {
     async getLGPriceArray(base: string, rel: string): Promise<GetLGPriceArrayResponse> {
         Logger.debug('LuxgateApi::getLGPriceArray called');
         try {
-            const password = "610fb5f228b6857fa91ada725033979a2a5e327b9a68ee6c59300d9d31c84718";
+            const password = LUXGATE_USER_PASSWORD;
             const timeline = 60;
             const response = await getLuxgatePriceArray({password, base, rel, timeline});
             if (response !== undefined)
